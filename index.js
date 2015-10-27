c68c39f374cb2441889ee09295108f6035f75a6f
cw0 <element4emerica@gmail.com>
2015-10-27 21:19:25 +0000

    added more logging capabilities, ready for testing and use with a db

--- index.js
+++ index.js
@@ -11,29 +11,32 @@ versionMessageIrssi = 'irssi v0.8.12 - running on CYGWIN_NT-6.1-WOW64 i686';
 versionMessageHexChat = 'VERSION HexChat 2.9.1 [x86] / Windows 8.1 [1.46GHz]';
 
 //initialize the client and connect to a channel
-client = new irc.Client('chat.freenode.net', 'kdrt', {
+client = new irc.Client('chat.freenode.net', 'swiffy', {
   channels: ['##chat']
 });
 
 //log successful connection to server
 client.addListener('registered', function (message) {
-  console.log(message);
+  console.log('kdr bot connected to server successfully');
+  //console.log(message);
 });
 
 //log kick messages
 client.addListener('kick', function (channel, nick, by, reason, message) {
   //TODO track statistics
-  console.log(nick + ' kicked by: '.green + by + ' for: '.green + reason);
+  console.log('%s kicked by: %s for: %s'.green, nick, by, green, reason);
 });
 
 //log any mode changes (+)
 client.addListener('+mode', function (channel, by, mode, argument, message) {
   //TODO track statistics
+  console.log('%s set mode: +%s on %s'.blue, by, mode, argument);
 });
 
 //log any mode changes (-)
 client.addListener('-mode', function (channel, by, mode, argument, message) {
   //TODO track statistics
+  console.log('%s set mode: -%s on %s'.blue, by, mode, argument);
 });
 
 //log messages (commented out for now)
@@ -43,16 +46,16 @@ client.addListener('message', function (from, to, message) {
 
 //log private messages
 client.addListener('pm', function (from, message) {
-  console.log(from + ' => me: '.yellow + message);
-})
+  console.log('%s => me: %s'.yellow, from, message);
+});
 
 //spoof the version
 client.addListener('ctcp-version', function (from, to, message) {
-  console.log('CTCP VERSION FROM '.red + from);
+  console.log('%s => me: CTCP VERSION'.red, from);
   client.ctcp(from, 'privmsg', versionMessageHexChat);
 });
 
 //handle errors
 client.addListener('error', function(message) {
-    console.log('error: ', message);
+  console.log('error: ', message);
 });
