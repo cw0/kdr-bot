bf9f30f3d3ca74372fc5f9f1c898cbe01f6f19d1
cw0 <element4emerica@gmail.com>
2015-10-29 01:19:43 -0400

    couchdb support added. all messages are now being saved.

--- bot.js
+++ bot.js
@@ -3,7 +3,9 @@
 //declare variables
 var
   irc = require('irc'),
-  colors = require('colors');
+  colors = require('colors'),
+  nano = require('nano')('http://localhost:5984'),
+  db = nano.db.use('kdr_bot_dev');
 
 exports.Bot = Bot;
 
@@ -12,18 +14,16 @@ function Bot(server, nick, channels) {
   this.server = server;
   this.nick = nick;
   this.channels = channels;
-  //this.fCount = 0;
-
-  var
-    versionMessageIrssi = 'irssi v0.8.12 - running on CYGWIN_NT-6.1-WOW64 i686',
-    versionMessageHexChat = 'VERSION HexChat 2.9.1 [x86] / Windows 8.1 [1.46GHz]';
-
 }
 
 Bot.prototype = {
   constructor: Bot,
   connect: function () {
-    var msgCount = 0;
+    var
+      self = this;
+      versionMessageIrssi = 'irssi v0.8.12 - running on CYGWIN_NT-6.1-WOW64 i686',
+      versionMessageHexChat = 'VERSION HexChat 2.9.1 [x86] / Windows 8.1 [1.46GHz]';
+
     this.client = new irc.Client(this.server, this.nick, {
       channels: this.channels
     });
@@ -32,28 +32,100 @@ Bot.prototype = {
       //console.log(message);
     });
     this.client.addListener('kick', function (channel, nick, by, reason, message) {
-      //TODO track statistics
-      console.log('%s kicked by: %s for: %s'.green, nick, by, reason);
+      data = {
+        type : 'kick',
+        channel : channel,
+        nick : nick,
+        by : by,
+        reason : reason,
+        message : message
+      };
+      db.insert(data, function (err, body) {
+        if (!err) {
+          console.log('%s kicked by: %s for: %s'.green, nick, by, reason);
+        } else {
+          console.log('error saving kick msg', data);
+        }
+      });
     });
     this.client.addListener('message', function (from, to, message) {
-      msgCount += 1;
-      console.log('msgCount: %s'.rainbow, msgCount.toString());
-      //console.log(from + ' => ' + to + ': ' + message);
+      var data = {
+        type : 'message',
+        from : from,
+        to : to,
+        message : message
+      };
+      db.insert(data, function (err, body) {
+        if (!err) {
+          //console.log(from + ' => ' + to + ': ' + message);
+        } else {
+          console.log('error saving msg', data);
+        }
+      });
     });
     this.client.addListener('+mode', function (channel, by, mode, argument, message) {
-      //TODO track statistics
-      console.log('%s set mode: +%s on %s'.blue, by, mode, argument);
+      var data = {
+        type : '+mode',
+        channel : channel,
+        by : by,
+        mode : mode,
+        on : argument,
+        message : message
+      };
+      db.insert(data, function (err, body) {
+        if (!err) {
+          console.log('%s set mode: +%s on %s'.blue, by, mode, argument);
+        } else {
+          console.log('error saving +mode', data);
+        }
+      });
     });
     this.client.addListener('-mode', function (channel, by, mode, argument, message) {
-      //TODO track statistics
-      console.log('%s set mode: -%s on %s'.blue, by, mode, argument);
+      var data = {
+        type : '-mode',
+        channel : channel,
+        by : by,
+        mode : mode,
+        on : argument,
+        message : message
+      };
+      db.insert(data, function (err, body) {
+        if (!err) {
+          console.log('%s set mode: -%s on %s'.blue, by, mode, argument);
+        } else {
+          console.log('error saving -mode', data);
+        }
+      });
     });
     this.client.addListener('pm', function (from, message) {
-      console.log('%s => me: %s'.yellow, from, message);
+      var data = {
+        type : 'pm',
+        from : from,
+        message : message
+      };
+      db.insert(data, function (err, body) {
+        if (!err) {
+          console.log('%s => me: %s'.yellow, from, message);
+        } else {
+          console.log('error saving pm', data);
+        }
+      });
     });
     this.client.addListener('ctcp-version', function (from, to, message) {
-      console.log('%s => me: CTCP VERSION'.red, from);
-      client.ctcp(from, 'privmsg', versionMessageHexChat);
+      var data = {
+        type : 'ctcp-version',
+        from : from,
+        to : to,
+        message : message
+      };
+      db.insert(data, function (err, body) {
+        if (!err) {
+          console.log('%s => me: CTCP VERSION'.red, from);
+        } else {
+          console.log('error saving ctcp-version', data);
+        }
+      });
+      self.client.ctcp(from, 'privmsg', versionMessageHexChat);
     });
     this.client.addListener('error', function (message) {
       console.log('error: ', message);
