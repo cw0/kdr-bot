4823ddade54345fae60921b8fc642e7774c82da6
cw0 <element4emerica@gmail.com>
2015-11-03 00:03:28 -0500

    basic gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Dependencies
+var
+  gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  notify = require('gulp-notify'),
+  livereload = require('gulp-livereload');
+
+//Task
+gulp.task('default', function () {
+  //listen for changes
+  livereload.listen();
+  //set up nodemon
+  nodemon({
+    //point to our app script
+    script: 'app.js',
+    ext: 'js'
+  }).on('restart', function () {
+    gulp.src('app.js')
+    .pipe(livereload())
+    .pipe(notify('reloading page please wait'));
+  });
+});
