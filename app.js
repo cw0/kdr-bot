4716ee556d8370e58a7fb0ca5b1713e316c80905
cw0 <element4emerica@gmail.com>
2015-11-03 18:58:33 +0000

    gulp fixes

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 //imports
-//TODO remove this gulp test
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ircBot = require('./bot');
 
+//routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+//init app and irc bot
 var app = express();
+var bot = new ircBot.Bot('chat.freenode.net', 'meatboy', 'meatboy', 'meatboy', ['##arguments']);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
