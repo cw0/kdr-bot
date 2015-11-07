e23fd6e565400df03117c92ff5bf8b7aa35e687f
cw0 <element4emerica@gmail.com>
2015-11-07 09:30:01 -0500

    jquery imported into jade templates, simple bot status checking.

---
+++ public/javascripts/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  hanldeBotStatus();
+});
+
+function hanldeBotStatus () {
+  var status = $('#bot-status').data('status');
+  console.log(status);
+
+  if (status == 'offline') {
+    $('body').addClass('offline');
+    $('body').removeClass('online');
+    $('html').addClass('offline');
+    $('html').removeClass('online');
+  } else {
+    $('body').addClass('online');
+    $('body').removeClass('offline');
+    $('html').addClass('online');
+    $('html').removeClass('offline');
+  }
+}
