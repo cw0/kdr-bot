183e182a985f96c61ffed4617dd53089c540c275
cw0 <element4emerica@gmail.com>
2015-11-15 23:55:08 -0500

    updated gulpfile, non stable

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,28 @@ var
   gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   notify = require('gulp-notify'),
+  browserify = require('browserify'),
+  reactify = require('reactify'),
+  source = require('vinyl-source-stream'),
   livereload = require('gulp-livereload');
 
-//Task
-gulp.task('default', function () {
+//watch jsx files for changes. If found run js task
+gulp.task('watch', function() {
+  gulp.watch("public/javascripts/src/**/*.jsx", ["js"]); //TODO Update to react directory
+});
+
+//build jsx
+gulp.task('js', function(){
+  browserify('./public/javascripts/src/app.jsx') //TODO Update to react directory
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('public/javascripts/build/'))
+    .pipe(notify('building jsx files'));
+});
+
+//livereload task
+gulp.task('livereload', function () {
   //listen for changes
   livereload.listen();
   //set up nodemon
@@ -20,3 +38,5 @@ gulp.task('default', function () {
     .pipe(notify('reloading page please wait'));
   });
 });
+
+gulp.task('default', ['watch', 'js', 'livereload']);
